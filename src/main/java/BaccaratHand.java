9317280ac7f61cdd3c5a8cd1a906f986e18cc2f2
rubayeaAlketbi <sc22rhsm@leeds.ac.uk>
2023-03-26 18:55:51 +0100

    Adding methods to Baccaract Hand

--- src/main/java/BaccaratHand.java
+++ src/main/java/BaccaratHand.java
@@ -30,14 +30,21 @@ public class BaccaratHand extends CardCollection {
 
     public boolean isNatural() {
         boolean naturalHand = false;
-        if (super.value() == 8 || super.value() == 9) {
+
+        if (super.size() == 2 && (super.value() == 8 || super.value() == 9)) {
             naturalHand = true;
         }
         return naturalHand;
     }
 
     // returns the string representation of the hand
+    @Override
     public String toString() {
-        return super.toString();
+        String handString = "";
+
+        for (Card card : cards) {
+            handString += card.toString() + " ";
+        }
+        return handString.trim();
     }
 }
