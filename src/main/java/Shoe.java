7612d3d8f1b514decf44c0906b427f557649df9e
rubayeaAlketbi <sc22rhsm@leeds.ac.uk>
2023-03-27 14:42:22 +0100

    Creating shoes  object

--- src/main/java/Shoe.java
+++ src/main/java/Shoe.java
@@ -1,8 +1,27 @@
+import java.util.LinkedList;
+import java.util.List;
+
 // TODO: Implement the Shoe class in this file
 public class Shoe extends CardCollection{
     // constructor of the shoe 
+    protected List <BaccaratCard> decks;
+
     public Shoe(int numDecks) {
-        return;
+        int deckIndex = 0;
+        if(numDecks == 8 || numDecks == 6){
+            // Create a new linked list of cards 
+            decks = new LinkedList<>();
+            while(deckIndex != numDecks){
+                for(Card.Suit s : Card.Suit.values()){
+                    for(Card.Rank r : Card.Rank.values()){
+                        decks.add(new BaccaratCard(r, s));
+                    }
+                }
+                deckIndex++;
+            }
+        }else{
+            throw new CardException("Invalid number of decks");
+        }
     }
     // returns the number of cards in the shoe
     public int size() {
