3e7c86e107dbb79aa57e5dcca093a0aa6bc22291
rubayeaAlketbi <sc22rhsm@leeds.ac.uk>
2023-04-08 11:37:43 +0100

    Creating the bsic baccarat program

--- src/main/java/Baccarat.java
+++ src/main/java/Baccarat.java
@@ -1,3 +1,22 @@
 public class Baccarat {
   // TODO: Implement your Baccarat simulation program here
+  /* 
+   Basic Main Prgoram 
+    Step 1 : Create a shoe with 6 decks --done--
+    Step 2 : Shuffle the shoe
+    Step 3 : Create two baccarat hands for the player and the banker --done--
+    Step 4 : Deal a card to the player and then the banker until both have two cards
+    Step 5: Display the content of the card and the value of the hand
+    Step 6 : Inform whether one of them has a natural hand.
+  */
+
+  // Create a shoe of 6 decks
+  private static Shoe baccaratShoe = new Shoe(6);
+
+  // Create the two hans for the player and the banker
+  private static BaccaratHand playerHand = new BaccaratHand();
+  private static BaccaratHand bankerHand = new BaccaratHand();
+
+  
+
 }
