128c522efa87ea5fae866b9250a1645703603a77
rubayeaAlketbi <sc22rhsm@leeds.ac.uk>
2023-04-24 13:42:59 +0100

    Fixed a small bug in calculating hand value

--- src/main/java/BaccaratHand.java
+++ src/main/java/BaccaratHand.java
@@ -22,7 +22,7 @@ public class BaccaratHand extends CardCollection {
     public int value() {
         int handValue = super.value();
         if (handValue >= 10) {
-            handValue = handValue - 10;
+            handValue = handValue % 10;
         }
         return handValue;
     }
